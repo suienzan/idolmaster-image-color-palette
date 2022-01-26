f18ac1bae7e9697b0634d53d8c9d069a7222fab9
Midori <suienzan@gmail.com>
2022-01-26 11:25:57 +0900

    change prettier config
    to make plugin works with pnpm

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const tailwindcss = require('prettier-plugin-tailwindcss');
+
+module.exports = {
+  plugins: [tailwindcss],
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
