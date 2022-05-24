9b3d38ca0986608359871249c3ffc9488d47fa31
Midori <suienzan@gmail.com>
2022-05-24 19:13:42 +0900

    remove unused

--- src/components/SimpleButton.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts" name="SimpleButton"></script>
-
-<template>
-  <button class="rounded-md bg-primary px-3 py-1 text-white shadow-sm shadow-neutral-800">
-    <slot />
-  </button>
-</template>
