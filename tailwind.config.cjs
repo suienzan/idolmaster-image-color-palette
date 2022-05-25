b87035d19bb33b721fe6e9e6606dcef5730e7b76
Midori <suienzan@gmail.com>
2022-05-25 11:18:23 +0900

    change dark theme background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const chroma = require('chroma.ts');
 const primary = '#BE1E3E';
 const accent = chroma.color(primary).brighter(1.5).hex();
 const light = '#FBFAFA';
-const dark = '#515558';
+const dark = '#3F3538';
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
